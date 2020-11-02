7d0d72bbeb4061fb1067619b9f7576aed60f6f9a
Yuntai Zeng <55123141+Goonertai@users.noreply.github.com>
2020-11-02 15:06:38 -0700

    fail test add

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -33,6 +33,6 @@ class CityListTest {
     void testCount(){
         CityList cityList = mockCityList();
 
-        assertEquals(1, cityList.countCities());
+        assertEquals(0, cityList.countCities());
     }
 }
